541cc7e7074bb985e68acac173439dc4b110172f
Systems engineer student | Jorge Daniel Ramírez Nieves <78396183+JorgeDanielRamirezNieves@users.noreply.github.com>
2021-03-26 12:25:47 -0500

    Add files via upload

---
+++ Homeworks/JAVA/P1_T037_JAVA_Methods/Main.java
@@ -0,0 +1,14 @@
+package USTA.fis;
+/*Author: Jorge Daniel Ramírez Nieves
+Date: 23 / March / 2021
+Description: This software use methods in your code*/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("----------------------------------");   //DATE: 23 / March / 2021
+        System.out.println("SoftMethod V1.0");
+        System.out.println("MAKER: Jorge Daniel Ramírez Nieves");
+        System.out.println("-DESCRIPTION: Use methods");
+        System.out.println("-----------------------------------");
+    }
+}
