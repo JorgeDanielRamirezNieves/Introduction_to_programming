3af911fb151baba1860a8da3d5b4ff201256ce00
Jorge Ramírez <jorge.ramirezn@usantoto.edu.co>
2021-06-03 20:07:53 -0500

    Upload 2021

--- Homeworks/JAVA [Intelli J Idea]/P1_T[Final]_ProjectTriqui/src/co/edu/usta/multiclass/matrixtriqui/Logic/SoftTriqui.java
+++ Homeworks/JAVA [Intelli J Idea]/P1_T[Final]_ProjectTriqui/src/co/edu/usta/multiclass/matrixtriqui/Logic/SoftTriqui.java
@@ -1,4 +1,428 @@
 package co.edu.usta.multiclass.matrixtriqui.Logic;
 
 public class SoftTriqui {
+    private char[][] tableTriqui;
+
+    public void createPlacesArray(int table) {
+        tableTriqui = new char[table][table];
+    }
+
+    public String setTableTriqui() {
+        StringBuilder graphicTable = new StringBuilder();
+        for (int i = 0; i < tableTriqui.length; i++) {
+            for (int j = 0; j < tableTriqui[0].length; j++) {
+                tableTriqui[i][j] = '▢';
+                graphicTable.append(tableTriqui[i][j]).append(" ");
+            }
+            graphicTable.append("\n");
+        }
+        return graphicTable.toString();
+    }
+
+    public int verificationTriqui() {
+        int availablePlaces=0;
+        for (int i=0; i<tableTriqui.length; i++) {
+            for (int j = 0; j < tableTriqui[0].length; j++) {
+                if (tableTriqui[i][j] == '▢') {
+                    availablePlaces++;
+                }
+            }
+        }
+        return availablePlaces;
+    }
+
+    public boolean verificationPlay(String rows, String columns) {
+        return tableTriqui[Integer.parseInt(rows)][Integer.parseInt(columns)] == '▢';
+    }
+
+    public void fillXPlay(String rows, String columns) {
+        if (verificationPlay(rows, columns)) {
+            tableTriqui[Integer.parseInt(rows)][Integer.parseInt(columns)] = 'X';
+        }
+    }
+
+    public void fillOPlay(String rows, String columns) {
+        if (verificationPlay(rows, columns)) {
+            tableTriqui[Integer.parseInt(rows)][Integer.parseInt(columns)] = 'O';
+        }
+    }
+
+    public String showTriqui() {
+        StringBuilder gameBoard= new StringBuilder();
+        for (int i = 0; i < tableTriqui.length; i++) {
+            for (int j = 0; j < tableTriqui[0].length; j++) {
+                gameBoard.append(tableTriqui[i][j]).append(" ");
+            }
+            gameBoard.append("\n");
+        }
+        return gameBoard.toString();
+    }
+
+    public int goalTriquiOp1(int availablePlaces, String value){ //Change if to for
+        int system=Integer.parseInt(value);
+        if(tableTriqui[0][0]=='X'){
+            if(tableTriqui[0][1]=='X'){
+                if(tableTriqui[0][2]=='X'){
+                    if(system==1){
+                        availablePlaces=-1;
+                    }
+                    if(system==2){
+                        if(tableTriqui[0][3]=='X'){
+                            if(tableTriqui[0][4]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+            if (tableTriqui[1][0]=='X'){
+                if(tableTriqui[2][0]=='X'){
+                    if(system==1){
+                        availablePlaces=-1;
+                    }
+                    if(system==2){
+                        if(tableTriqui[3][0]=='X'){
+                            if(tableTriqui[4][0]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+            if (tableTriqui[1][1]=='X'){
+                if(tableTriqui[2][2]=='X'){
+                    if(system==1){
+                        availablePlaces=-1;
+                    }
+                    if(system==2){
+                        if(tableTriqui[3][3]=='X'){
+                            if(tableTriqui[4][4]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(tableTriqui[0][2]=='X'){
+            if(tableTriqui[1][2]=='X'){
+                if(tableTriqui[2][2]=='X'){
+                    if(system==1){
+                        availablePlaces=-1;
+                    }
+                    if(system==2){
+                        if(tableTriqui[3][2]=='X'){
+                            if(tableTriqui[4][2]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+            if(system==1){
+                if(tableTriqui[1][1]=='X'){
+                    if(tableTriqui[2][0]=='X'){
+                        availablePlaces=-1;
+                    }
+                }
+            }
+        }
+        if(tableTriqui[0][1]=='X'){
+            if(tableTriqui[1][1]=='X'){
+                if(tableTriqui[2][1]=='X'){
+                    if(system==1){
+                        availablePlaces=-1;
+                    }
+                    if(system==2){
+                        if(tableTriqui[3][1]=='X'){
+                            if(tableTriqui[4][1]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(tableTriqui[1][0]=='X'){
+            if(tableTriqui[1][1]=='X'){
+                if(tableTriqui[1][2]=='X'){
+                    if(system==1){
+                        availablePlaces=-1;
+                    }
+                    if(system==2){
+                        if(tableTriqui[1][3]=='X'){
+                            if(tableTriqui[1][4]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(tableTriqui[2][0]=='X'){
+            if(tableTriqui[2][1]=='X'){
+                if(tableTriqui[2][2]=='X'){
+                    if(system==1){
+                        availablePlaces=-1;
+                    }
+                    if(system==2){
+                        if(tableTriqui[2][3]=='X'){
+                            if(tableTriqui[2][4]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(system==2){
+            if(tableTriqui[0][4]=='X'){
+                if(tableTriqui[1][4]=='X'){
+                    if(tableTriqui[2][4]=='X'){
+                        if(tableTriqui[3][4]=='X'){
+                            if(tableTriqui[4][4]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+                if(tableTriqui[1][3]=='X'){
+                    if(tableTriqui[2][2]=='X'){
+                        if(tableTriqui[3][1]=='X'){
+                            if(tableTriqui[4][0]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+            if(tableTriqui[0][3]=='X'){
+                if(tableTriqui[1][3]=='X'){
+                    if(tableTriqui[2][3]=='X'){
+                        if(tableTriqui[3][3]=='X'){
+                            if(tableTriqui[4][3]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+            if(tableTriqui[3][0]=='X'){
+                if(tableTriqui[3][1]=='X'){
+                    if(tableTriqui[3][2]=='X'){
+                        if(tableTriqui[3][3]=='X'){
+                            if(tableTriqui[3][4]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+            if(tableTriqui[4][0]=='X'){
+                if(tableTriqui[4][1]=='X'){
+                    if(tableTriqui[4][2]=='X'){
+                        if(tableTriqui[4][3]=='X'){
+                            if(tableTriqui[4][4]=='X'){
+                                availablePlaces=-1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(tableTriqui[0][0]=='O'){
+            if(tableTriqui[0][1]=='O'){
+                if(tableTriqui[0][2]=='O'){
+                    if(system==1){
+                        availablePlaces=-2;
+                    }
+                    if(system==2){
+                        if(tableTriqui[0][3]=='O'){
+                            if(tableTriqui[0][4]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (tableTriqui[1][0]=='O'){
+                if(tableTriqui[2][0]=='O'){
+                    if(system==1){
+                        availablePlaces=-2;
+                    }
+                    if(system==2){
+                        if(tableTriqui[3][0]=='O'){
+                            if(tableTriqui[4][0]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (tableTriqui[1][1]=='O'){
+                if(tableTriqui[2][2]=='O'){
+                    if(system==1){
+                        availablePlaces=-2;
+                    }
+                    if(system==2){
+                        if(tableTriqui[3][3]=='O'){
+                            if(tableTriqui[4][4]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(tableTriqui[0][2]=='O'){
+            if(tableTriqui[1][2]=='O'){
+                if(tableTriqui[2][2]=='O'){
+                    if(system==1){
+                        availablePlaces=-2;
+                    }
+                    if(system==2){
+                        if(tableTriqui[3][2]=='O'){
+                            if(tableTriqui[4][2]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+            if(system==1){
+                if(tableTriqui[1][1]=='O'){
+                    if(tableTriqui[2][0]=='O'){
+                        availablePlaces=-2;
+                    }
+                }
+            }
+        }
+        if(tableTriqui[0][1]=='O'){
+            if(tableTriqui[1][1]=='O'){
+                if(tableTriqui[2][1]=='O'){
+                    if(system==1){
+                        availablePlaces=-2;
+                    }
+                    if(system==2){
+                        if(tableTriqui[3][1]=='O'){
+                            if(tableTriqui[4][1]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(tableTriqui[1][0]=='O'){
+            if(tableTriqui[1][1]=='O'){
+                if(tableTriqui[1][2]=='O'){
+                    if(system==1){
+                        availablePlaces=-2;
+                    }
+                    if(system==2){
+                        if(tableTriqui[1][3]=='O'){
+                            if(tableTriqui[1][4]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(tableTriqui[2][0]=='O'){
+            if(tableTriqui[2][1]=='O'){
+                if(tableTriqui[2][2]=='O'){
+                    if(system==1){
+                        availablePlaces=-2;
+                    }
+                    if(system==2){
+                        if(tableTriqui[2][3]=='O'){
+                            if(tableTriqui[2][4]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(system==2){
+            if(tableTriqui[0][4]=='O'){
+                if(tableTriqui[1][4]=='O'){
+                    if(tableTriqui[2][4]=='O'){
+                        if(tableTriqui[3][4]=='O'){
+                            if(tableTriqui[4][4]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+                if(tableTriqui[1][3]=='O'){
+                    if(tableTriqui[2][2]=='O'){
+                        if(tableTriqui[3][1]=='O'){
+                            if(tableTriqui[4][0]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+            if(tableTriqui[0][3]=='O'){
+                if(tableTriqui[1][3]=='O'){
+                    if(tableTriqui[2][3]=='O'){
+                        if(tableTriqui[3][3]=='O'){
+                            if(tableTriqui[4][3]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+            if(tableTriqui[3][0]=='O'){
+                if(tableTriqui[3][1]=='O'){
+                    if(tableTriqui[3][2]=='O'){
+                        if(tableTriqui[3][3]=='O'){
+                            if(tableTriqui[3][4]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+            if(tableTriqui[4][0]=='O'){
+                if(tableTriqui[4][1]=='O'){
+                    if(tableTriqui[4][2]=='O'){
+                        if(tableTriqui[4][3]=='O'){
+                            if(tableTriqui[4][4]=='O'){
+                                availablePlaces=-2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return availablePlaces;
+    }
+
+    public int verificationX() {
+        int quantityX=0;
+        for (char[] chars : tableTriqui) {
+            for (int j = 0; j < tableTriqui[0].length; j++) {
+                if (chars[j] == 'X') {
+                    quantityX++;
+                }
+            }
+        }
+        return quantityX;
+    }
+    public int verificationO() {
+        int quantityO=0;
+        for (char[] chars : tableTriqui) {
+            for (int j = 0; j < tableTriqui[0].length; j++) {
+                if (chars[j] == 'O') {
+                    quantityO++;
+                }
+            }
+        }
+        return quantityO;
+    }
 }
--- Homeworks/JAVA [Intelli J Idea]/P1_T[Final]_ProjectTriqui/src/co/edu/usta/multiclass/matrixtriqui/view/FormsView.java
+++ Homeworks/JAVA [Intelli J Idea]/P1_T[Final]_ProjectTriqui/src/co/edu/usta/multiclass/matrixtriqui/view/FormsView.java
@@ -1,4 +1,116 @@
 package co.edu.usta.multiclass.matrixtriqui.view;
 
+import co.edu.usta.multiclass.matrixtriqui.Logic.SoftTriqui;
+import javax.swing.*;
+
 public class FormsView {
+
+    public static void main(String[] args){
+        SoftTriqui mySoftTriqui = new SoftTriqui();
+        String value, rows, columns;
+        int table=0, availablePlaces;
+
+        value= JOptionPane.showInputDialog("""
+                Welcome to SoftTriqui, please, select an option:
+                
+                0- Exit
+                1- Play on a 3x3 table
+                2- Play on a 5x5 table""");
+        while(Integer.parseInt(value)<0||Integer.parseInt(value)>2){
+            value=JOptionPane.showInputDialog(null, """
+                    Your option is wrong, please type/input again your answer. Remember, the number should be between 0 and 2+
+
+                    0- Exit
+                    1- Play on a 3x3 table
+                    2- Play on a 5x5 table""", "Error x_x",JOptionPane.ERROR_MESSAGE);
+        }
+        if(Integer.parseInt(value)==1){
+            table=3;
+            mySoftTriqui.createPlacesArray(table);
+        }
+        if(Integer.parseInt(value)==2) {
+            table = 5;
+            mySoftTriqui.createPlacesArray(table);
+        }
+        availablePlaces=table*table;
+        JOptionPane.showMessageDialog(null, mySoftTriqui.setTableTriqui());
+
+        while(availablePlaces<-2||availablePlaces>0){
+            if(availablePlaces>0){
+                rows=JOptionPane.showInputDialog(null,"Please, type/input the row that you want insert your position", "Player 1 - X", JOptionPane.INFORMATION_MESSAGE);
+                while(Integer.parseInt(rows)<0||Integer.parseInt(rows)>=table){
+                    rows=JOptionPane.showInputDialog(null,"Uh, you should only input/type numbers between 0 and "+(table-1),"Player 1 -X | Error x_x",JOptionPane.INFORMATION_MESSAGE);
+                }
+                columns=JOptionPane.showInputDialog(null,"Please, type/input the column that you want insert your position", "Player 1 - X",JOptionPane.INFORMATION_MESSAGE);
+                while(Integer.parseInt(columns)<0||Integer.parseInt(columns)>=table){
+                    columns=JOptionPane.showInputDialog(null,"Uh, you should only input/type numbers between 0 and "+(table-1),"Player 1 -X | Error x_x",JOptionPane.INFORMATION_MESSAGE);
+                }
+                while(!mySoftTriqui.verificationPlay(rows, columns)) {
+                    JOptionPane.showMessageDialog(null, "The place isn't available!", "Hey! We've an error x_x", JOptionPane.INFORMATION_MESSAGE);
+                    rows = JOptionPane.showInputDialog(null, "Please, type/input the row that you want insert your position", "Player 1 - X",JOptionPane.INFORMATION_MESSAGE);
+                    while (Integer.parseInt(rows) < 0 || Integer.parseInt(rows) >= table) {
+                        rows = JOptionPane.showInputDialog(null, "Uh, you should only input/type numbers between 0 and " + (table - 1), "Player 1 -X | Error x_x",JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    columns = JOptionPane.showInputDialog(null, "Please, type/input the column that you want insert your position", "Player 1 - X",JOptionPane.INFORMATION_MESSAGE);
+                    while (Integer.parseInt(columns) < 0 || Integer.parseInt(columns) >= table) {
+                        columns = JOptionPane.showInputDialog(null, "Uh, you should only input/type numbers between 0 and " + (table - 1), "Player 1 -X | Error x_x",JOptionPane.INFORMATION_MESSAGE);
+                    }
+                }
+                mySoftTriqui.fillXPlay(rows, columns);
+                availablePlaces=mySoftTriqui.verificationTriqui();
+                //System.out.println(availablePlaces);
+                JOptionPane.showMessageDialog(null, mySoftTriqui.showTriqui());
+            }
+            availablePlaces=mySoftTriqui.goalTriquiOp1(availablePlaces, value);
+            //System.out.println(availablePlaces);
+            if(availablePlaces>0){
+                rows=JOptionPane.showInputDialog(null,"Please, type/input the row that you want insert your position", "Player 1 - O",JOptionPane.INFORMATION_MESSAGE);
+                while(Integer.parseInt(rows)<0||Integer.parseInt(rows)>=table){
+                    rows=JOptionPane.showInputDialog(null,"Uh, you should only input/type numbers between 0 and "+(table-1),"Player 1 -O | Error x_x",JOptionPane.INFORMATION_MESSAGE);
+                }
+                columns=JOptionPane.showInputDialog(null,"Please, type/input the column that you want insert your position", "Player 1 - O",JOptionPane.INFORMATION_MESSAGE);
+                while(Integer.parseInt(columns)<0||Integer.parseInt(columns)>=table){
+                    columns=JOptionPane.showInputDialog(null,"Uh, you should only input/type numbers between 0 and "+(table-1),"Player 1 -O | Error x_x",JOptionPane.INFORMATION_MESSAGE);
+                }
+                while(!mySoftTriqui.verificationPlay(rows, columns)){
+                    JOptionPane.showMessageDialog(null, "The place isn't available!", "Hey! We've an error x_x",JOptionPane.INFORMATION_MESSAGE);
+                    rows=JOptionPane.showInputDialog(null,"Please, type/input the row that you want insert your position","Player 1 - O", JOptionPane.INFORMATION_MESSAGE);
+                    while(Integer.parseInt(rows)<0||Integer.parseInt(rows)>=table){
+                        rows=JOptionPane.showInputDialog(null,"Uh, you should only input/type numbers between 0 and "+(table-1),"Player 1 -O | Error x_x",JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    columns=JOptionPane.showInputDialog(null,"Please, type/input the column that you want insert your position", "Player 1 - O",JOptionPane.INFORMATION_MESSAGE);
+                    while(Integer.parseInt(columns)<0||Integer.parseInt(columns)>=table){
+                        columns=JOptionPane.showInputDialog(null,"Uh, you should only input/type numbers between 0 and "+(table-1),"Player 1 -O | Error x_x",JOptionPane.INFORMATION_MESSAGE);
+                    }
+                }
+                mySoftTriqui.fillOPlay(rows, columns);
+                availablePlaces=mySoftTriqui.verificationTriqui();
+                JOptionPane.showMessageDialog(null, mySoftTriqui.showTriqui());
+            }
+        }
+        if(availablePlaces==0){
+            JOptionPane.showMessageDialog(null,"What a pity :(. Nobody wins!" +
+                    "\n" +
+                    "\nAvailable places: "+mySoftTriqui.verificationTriqui()+
+                    "\nQuantity of X: "+mySoftTriqui.verificationX() +
+                    "\nQuantity of O: "+mySoftTriqui.verificationO()+
+                    "\nGame mode: "+table+"x"+table );
+        }
+        if(availablePlaces==-1){
+            JOptionPane.showMessageDialog(null,"Player 1 - X wins! Congratulations! :D" +
+                    "\n" +
+                    "\nAvailable places: "+mySoftTriqui.verificationTriqui()+
+                    "\nQuantity of X: "+mySoftTriqui.verificationX() +
+                    "\nQuantity of O: "+mySoftTriqui.verificationO()+
+                    "\nGame mode: "+table+"x"+table );
+        }
+        if(availablePlaces==-2){
+            JOptionPane.showMessageDialog(null,"Player 2 - O wins! Congratulations! :D" +
+                    "\n" +
+                    "\nAvailable places: "+mySoftTriqui.verificationTriqui()+
+                    "\nQuantity of X: "+mySoftTriqui.verificationO() +
+                    "\nQuantity of O: "+mySoftTriqui.verificationX()+
+                    "\nGame mode: "+table+"x"+table );
+        }
+    }
 }
